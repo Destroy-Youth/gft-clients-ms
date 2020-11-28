bf7d284138fdf0ca5b82a0ad56ec4f4ce2e54456
Equihua <luis.herrerae@outlook.com>
2020-11-27 18:13:26 -0600

    Feign client deserialization config

--- src/main/java/dev/gft/example/clients/ClientsApplication.java
+++ src/main/java/dev/gft/example/clients/ClientsApplication.java
@@ -3,9 +3,13 @@ package dev.gft.example.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ClientsApplication {
 
 	public static void main(String[] args) {
