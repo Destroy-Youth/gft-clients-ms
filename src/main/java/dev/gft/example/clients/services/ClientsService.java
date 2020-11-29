9f4b547918a884f7a4be909f41dfe47d153ee4b6
Equihua <luis.herrerae@outlook.com>
2020-11-29 17:20:49 -0600

    java 11 features removed

--- src/main/java/dev/gft/example/clients/services/ClientsService.java
+++ src/main/java/dev/gft/example/clients/services/ClientsService.java
@@ -2,6 +2,7 @@ package dev.gft.example.clients.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -23,19 +24,19 @@ public class ClientsService implements ICRUD<ClientDTO, Long> {
     @Override
     public ClientDTO findOneById(Long id) {
         Optional<EntityModel<ClientDTO>> client = Optional.ofNullable(dataClient.findOneById(id).getBody());
-        return client.orElseThrow().getContent();
+        return client.orElseThrow(NoSuchElementException::new).getContent();
     }
 
     @Override
     public List<ClientDTO> findAll() {
         Optional<CollectionModel<ClientDTO>> clients = Optional.ofNullable(dataClient.findAllClients().getBody());
-        return new ArrayList<>(clients.orElseThrow().getContent());
+        return new ArrayList<>(clients.orElseThrow(NoSuchElementException::new).getContent());
     }
 
     @Override
     public ClientDTO createOne(ClientDTO newRegistry) {
         Optional<EntityModel<ClientDTO>> client = Optional.ofNullable(dataClient.createOne(newRegistry).getBody());
-        return client.orElseThrow().getContent();
+        return client.orElseThrow(NoSuchElementException::new).getContent();
     }
 
     @Override
@@ -43,7 +44,7 @@ public class ClientsService implements ICRUD<ClientDTO, Long> {
         findOneById(id);
         Optional<EntityModel<ClientDTO>> client = Optional
                 .ofNullable(dataClient.updateOne(id, updatedRegistry).getBody());
-        return client.orElseThrow().getContent();
+        return client.orElseThrow(NoSuchElementException::new).getContent();
     }
 
     @Override
@@ -53,7 +54,7 @@ public class ClientsService implements ICRUD<ClientDTO, Long> {
 
     public List<String> getAllClientFullNames(String search) {
 
-        Predicate<ClientDTO> filterByNameContains = client -> search.isBlank() ? Boolean.TRUE
+        Predicate<ClientDTO> filterByNameContains = client -> search.isEmpty() ? Boolean.TRUE
                 : client.getFirstName().toLowerCase().contains(search);
 
         return findAll().stream().filter(filterByNameContains).map(client -> new StringBuilder()
